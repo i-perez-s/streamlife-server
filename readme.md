46f40fbf35a05c71069a4e73fa2d8e5405261466
Z3hcans <izan.perezsanchez@gmail.com>
2021-01-29 13:15:01 +0100

    empezar login

---
+++ readme.md
@@ -0,0 +1,31 @@
+# About
+Repository created for the Typescript with node and Express.Js article in Medium.
+
+Minimal boilerplate to start a Express.js project using TypeScript.
+
+## Quick Start
+To install this dependency use:
+
+```
+npm install
+```
+To run ther server in development mode:
+```
+npm run dev
+```
+To run ther server in production mode:
+```
+npm run prod
+```
+To clean the folder dist (here is it the project compiled):
+```
+npm run clean
+```
+To execute the project when is compiled:
+```
+npm start
+```
+To compile the project:
+```
+npm run tsc
+```
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import { json } from 'body-parser'
+
+
+import { port, uriMongo as url } from './config'
+import { app as authRouter } from './routes/index'
+import './auth/auth'
+
+
+// Create a new express application instance
+const app: express.Application = express();
+app.use(json())
+
+
+app.use(authRouter);
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+/* mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, res) => {
+    if (err) throw err
+    console.log('>>>> Db conected')
+}) */
+
+app.listen(port, function () {
+  console.log('Server on port:', port);
+});
