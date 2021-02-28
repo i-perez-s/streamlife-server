311772d1aac6630707d0a6b2754fba885635ca40
Zehcnas <izan.perezsanchez@gmail.com>
2021-02-28 10:59:20 +0100

    change the way to connect to the db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 
 export const connectDb = async() => {
-    await mongoose.connect('mongodb://localhost:27017/portafolio', { useNewUrlParser: true })
-    .then(() => {
-        console.log('>>>>>>>>>>>>> DB connected')
-            //creacion del server
-    })
-    .catch(err => console.log(err))
-
+    try {
+        if (process.env.URI_MONGO === undefined) {
+            process.env.URI_MONGO = ''
+        }
+        await mongoose.connect(process.env.URI_MONGO, { useNewUrlParser: true })
+        console.log('>>>Db connected')
+    } catch (error) {
+        console.log(error)
+        throw new Error("Error connecting to the db")
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,16 @@ import setUpServer from './config/setUptServer'
 
 const [app, io, nms] = setUpServer()
 
+//stream server
 nms.run()
 
-connectDb()
+console.log(process.env.URI_MONGO)
 
+connectDb()
 
 app.listen(process.env.EXPRESS_PORT, function () {
   console.log('Express server on port:', process.env.EXPRESS_PORT);
 });
+
 
 module.exports = { io }
