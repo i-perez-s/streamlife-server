1a1ef4ad66a89408af73c28a6a65a6278d96eacd
Z3hcans <izan.perezsanchez@gmail.com>
2021-05-06 14:24:51 +0200

    change user socket

--- src/sockets/controller.ts
+++ src/sockets/controller.ts
@@ -8,18 +8,16 @@ export const socketController = async (socket: any) => {
     return socket.disconnect();
   }
 
-  console.log(user.username, "se unio");
   socket.on("disconnect", () => console.log(user.username, "se desconecto"));
-
   socket.join(socket.handshake.headers["idstream"]);
 
   socket.on("send-message", (message: any) => {
+    console.log(message.chat);
     const messageCreated = new Message(message.text, user._id);
-    socket.to(message.chat).emit("send-message", messageCreated);
+    socket.emit("send-message", messageCreated);
   });
   socket.on("send-emote", async (message: any) => {
     //check if it's an emote
-
     const emoteId = message.text;
     const emote = await Emote.findById(emoteId);
     if (!emote) {
@@ -27,11 +25,6 @@ export const socketController = async (socket: any) => {
     } else {
       message.text = emote.urlEmote;
     }
-    const messageCreated = new Message(message.text, user._id);
-    console.log(messageCreated);
-    socket.to(message.chat).emit("send-message", messageCreated);
-  });
-  socket.on("send-message", async (message: any) => {
     const messageCreated = new Message(message.text, user._id);
     console.log(messageCreated);
     socket.to(message.chat).emit("send-message", messageCreated);
